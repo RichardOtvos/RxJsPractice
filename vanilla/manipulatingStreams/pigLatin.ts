2b08d3a6ef15db6ad5832b4e39d35f6909b84145
Richard Otvos <richard.otvos@cloudera.com>
2019-06-09 13:08:58 +0200

    Chapter2 ex1

--- vanilla/manipulatingStreams/pigLatin.ts
+++ vanilla/manipulatingStreams/pigLatin.ts
@@ -1,2 +1,25 @@
+import { fromEvent, from } from "rxjs";
+import { map, mergeMap, tap, reduce } from "rxjs/operators";
+
 let textbox = <HTMLElement>document.querySelector('#text-input');
 let results = <HTMLElement>document.querySelector('#results');
+let keyUp$ = fromEvent(textbox, 'keyup');
+
+function pigLatinify(word) {
+    if (word.length < 2) {
+        return word;
+    }
+
+    return `${word.slice(1)}-${word[0].toLowerCase()}ay`;
+}
+
+keyUp$.pipe(
+    map(event => (<HTMLTextAreaElement>event.target).value),
+    mergeMap(sentence =>
+        from(sentence.split(/\s+/))
+            .pipe(
+                map(pigLatinify),
+                reduce((bigStr, newWord) => `${bigStr} ${newWord}`, '')
+            )
+    ),
+).subscribe(console.log);
