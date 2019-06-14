d9e85bebedcbd03bc530f1f0bde064fb106111e9
Richard Otvos <richard.otvos@cloudera.com>
2019-06-14 13:41:01 +0200

    Adds stock ticker example

--- vanilla/advancedAsync/stocks.ts
+++ vanilla/advancedAsync/stocks.ts
@@ -1,7 +1,7 @@
 declare var Chart;
 
 import { merge, fromEvent, of, combineLatest } from 'rxjs';
-import { scan, map } from 'rxjs/operators';
+import { scan, map, pluck, tap } from 'rxjs/operators';
 import { webSocket } from 'rxjs/webSocket';
 
 let config = {
@@ -53,3 +53,79 @@ let abcEl = <HTMLElement>document.querySelector('.abc');
 let defEl = <HTMLElement>document.querySelector('.def');
 let ghiEl = <HTMLElement>document.querySelector('.ghi');
 let jklEl = <HTMLElement>document.querySelector('.jkl');
+
+function makeCheckboxStream(el, ticker) {
+  return merge(
+    fromEvent(el, 'change'),
+    of({ target: { checked: true } })
+  ).pipe(
+    pluck('target', 'checked'),
+    map(isEnabled => ({
+      isEnabled,
+      stock: ticker
+    }))
+  )
+}
+
+function separateDataByStocks(dataSet) {
+  let tempRes = dataSet.reduce((prev, datum) => {
+    datum.forEach(d => {
+      if (!prev[d.label]) {
+        prev[d.label] = []
+      }
+      prev[d.label].push(d.price);
+    })
+    return prev;
+  }, {})
+
+  return Object.keys(tempRes).map(key => {
+    // convert into something chart.js can read​
+    return {
+      label: key,
+      data: tempRes[key],
+      fill: false,
+      backgroundColor: colorMap[key],
+      borderColor: colorMap[key]
+    }
+  });
+
+}
+
+let endpoint = 'ws://localhost:3000/api/advancedAsync/stock-ws';
+let stockStream$ = webSocket(endpoint)
+  .pipe(
+    scan((acc, next) => {
+      acc.push(next);
+      if (acc.length > 10) {
+        acc.shift()
+      }
+      return acc;
+    }, []),
+    map(separateDataByStocks)
+  );
+
+let settings$ = combineLatest(
+  makeCheckboxStream(abcEl, 'abc'),
+  makeCheckboxStream(defEl, 'def'),
+  makeCheckboxStream(ghiEl, 'ghi'),
+  makeCheckboxStream(jklEl, 'jkl'),
+  (...filterRes) => {
+    return filterRes
+      .filter(f => f.isEnabled)
+      .map(f => f.stock)
+  }
+);
+
+combineLatest(
+  settings$,
+  stockStream$,
+  (enabledStocks, stockUpdates) => ({ enabledStocks, stockUpdates })
+).pipe(
+  map(({ enabledStocks, stockUpdates }) => {
+    console.log(enabledStocks, stockUpdates)
+    return stockUpdates.filter(stockHistory => enabledStocks.includes(stockHistory.label.toLowerCase()));
+  }),
+).subscribe(newDataSet => {
+  config.data.datasets = newDataSet;
+  stockChart.update();
+}, err => console.error(err));
